754a5c5705e6912e45623bfba0a874f9593bb55d
mv-stns <21103251+mv-stns@users.noreply.github.com>
2022-09-14 14:24:38 +0200

    Close T-28 deleted unneeded html index and added tailwindcss files/links

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,10 +1,14 @@
+<?php
+  $userName = "Marcus";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="css/main.css">
+  <title>v.CLOUD Personal Portfolio</title>
   <!--
     todolist for heightapp:
       // T-28 add tailwindcss to the header
@@ -12,6 +16,8 @@
   -->
 </head>
 <body>
-  <?php echo "Hello World" ?>
+  <h1 class="font-black tracking-tight font-sans text-3xl">
+    Hello <?php $userName ?>
+  </h1>
 </body>
 </html>
