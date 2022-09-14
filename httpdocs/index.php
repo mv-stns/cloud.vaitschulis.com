0e35ef69e5f19cc3316a7947a9329a02fe57edbf
mv-stns <21103251+mv-stns@users.noreply.github.com>
2022-09-14 18:30:44 +0200

    added header, included alpinejs, made fancy frontend anim with blurry stuff thankfully to @FonsMans

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -2,6 +2,7 @@
 <!--
     todolist for heightapp:
       // T-27 start building basic components
+      // T-30 build main gradient component
   -->
 <html lang="en">
 
@@ -13,15 +14,11 @@
   <title>v.CLOUD Personal Portfolio</title>
   <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <script src="./scripts/components.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 
-<body>
-  <!--
-  When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars
-
-  Open: "fixed inset-0 z-40 overflow-y-auto", Closed: ""
--->
-  <header class="bg-white shadow-sm lg:static lg:overflow-y-visible" x-state:on="Open" x-state:off="Closed" :class="{ 'fixed inset-0 z-40 overflow-y-auto': open }" x-data="Components.popover({ open: false, focus: false })" x-init="init()" @keydown.escape="onEscape" @close-popover-group.window="onClosePopoverGroup">
+<body class="relative">
+<header class="top-0 absolute w-full z-[50] bg-white backdrop-blur-xl shadow-xl lg:overflow-y-visible" x-state:on="Open" x-state:off="Closed" :class="{ 'fixed inset-0 z-40 overflow-y-auto': open }" x-data="Components.popover({ open: false, focus: false })" x-init="init()" @keydown.escape="onEscape" @close-popover-group.window="onClosePopoverGroup">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
         <div class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
@@ -140,9 +137,57 @@ Heroicon name: outline/x" x-state:on="Menu open" x-state:off="Menu closed" class
     </nav>
   </header>
 
-  <main>
+  <main class="relative w-screen h-screen overflow-hidden">
+    <div class="relative bg-[#252525]">
+      <img src="./assets/img/blobs/subtle_grain.png" class="absolute inset-0 w-full h-full object-cover z-[1] opacity-10 mix-blend-darken" />
+      <!-- <div class="absolute inset-0 bg-black/50 mix-blend-darken"></div> -->
+
+      <svg viewBox="0 0 1512 982" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_878_1737)">
+          <rect width="100%" height="100%" fill="#252525" />
+          <g filter="url(#filter0_f_878_1737)">
+            <path d="M984 217.5C984 359 658.5 387.756 583 456.5C507.5 525.244 158.359 964 -19.5 964C-162.829 1141.33 -139 1008 -52.805 933.553C-174.626 933.553 -103.184 -0.252945 -103.184 -97.0044C-103.184 -193.756 355.031 -40.2318 476.852 -40.2318C763.466 -126.742 984 120.749 984 217.5Z" fill="#A390CE" />
+          </g>
+          <g filter="url(#filter1_f_878_1737)">
+            <ellipse cx="983.5" cy="-15.5" rx="332.5" ry="263.5" fill="#8EADD9" />
+          </g>
+
+
+          <g id="blobToMouse" class="mix-blend-color-dodge" filter="url(#filter2_f_878_1737)">
+            <path d="M145.315 -7.65351C122.208 154.962 385.107 134.277 519.445 103.607C555.57 82.7502 509.545 -26.117 443.829 -95.7139C378.113 -165.311 548.881 -294.712 484.481 -387.158C420.081 -479.603 174.199 -210.923 145.315 -7.65351Z" fill="#F838CD" />
+            <path d="M233.221 -116.853C162.602 -177.782 -362.623 426.665 -182.463 523.08C-19.1196 610.495 401.825 195.226 451.222 153.295C512.968 100.882 321.496 -40.6913 233.221 -116.853Z" fill="#7A81BD" />
+            <path d="M98.7067 -164.626C43.8658 -288.255 -143.801 -301.321 -235.873 -301.321C-327.946 -301.321 -513.6 -280.817 -519.637 -198.8C-527.184 -96.2787 -414.484 -88.7404 -414.484 128.363C-414.484 345.467 -216.754 206.259 23.2375 206.259C215.231 206.259 153.548 -40.9977 98.7067 -164.626Z" fill="#F1972C" />
+          </g>
+
+        </g>
+        <defs>
+          <filter id="filter0_f_878_1737" x="-424" y="-429" width="1708" height="1780.99" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_878_1737" />
+          </filter>
+          <filter id="filter1_f_878_1737" x="351" y="-579" width="1265" height="1127" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_878_1737" />
+          </filter>
+          <filter id="filter2_f_878_1737" x="-960" y="-845.921" width="1932.12" height="1821.06" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_878_1737" />
+          </filter>
+          <clipPath id="clip0_878_1737">
+            <rect width="1512" height="982" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+
+    </div>
+
+    <img class="absolute block w-auto h-8 bottom-10 right-10" src="./assets/img/logo/white_logo.svg" alt="v.Cloud">
 
   </main>
+
   <footer></footer>
 
   <!--
@@ -158,6 +203,11 @@ Heroicon name: outline/x" x-state:on="Menu open" x-state:off="Menu closed" class
           el.src = data.avatar_url;
         });
       });
+    // center blobToMouse on mouse
+    document.addEventListener('mousemove', (e) => {
+      const blob = document.getElementById('blobToMouse');
+      blob.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    });
   </script>
 </body>
 
