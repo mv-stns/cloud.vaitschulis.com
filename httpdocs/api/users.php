78d4ec0f5edd4265da844078b1ab8ae3cb4e4b62
mv-stns <21103251+mv-stns@users.noreply.github.com>
2022-09-23 10:53:01 +0200

    created login/registration api

--- httpdocs/api/users.php
+++ httpdocs/api/users.php
@@ -1,35 +1,124 @@
 <?php
-// T-33 create user fetch api
 
-// import globalSettings.php
-require '../config/globalSettings.php';
+// autloader from vendor
+require __DIR__ . '/../../vendor/autoload.php';
 
-// load db data from .env
-$server = $_ENV['DB_DEV_HOST'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
-$dbport = $_ENV['DB_PORT'];
+$rootPath = __DIR__ . '/../../';
 
-// await get request from index.php
-// check if user exists, if not create user
+// get env variables
+$dotenv = Dotenv\Dotenv::createImmutable($rootPath);
+$dotenv->load();
 
-if ($_POST['action'] == 'checkUser') {
-    // create connection
-    $conn = new mysqli($server, $username, $password, $dbname, $dbport);
-    // check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    // prepare statement
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-    // bind parameters
-    $stmt->bind_param("ss", $username, $password);
-    // set parameters and execute
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $stmt->execute();
-    // close connection
-    $stmt->close();
-    $conn->close();
+$db_host = $_ENV['DB_DEV_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_password = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
+
+// connect to database mariadb
+$connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$username = $_POST['email'];
+$password = $_POST['password'];
+
+// if post 'action' is set to 'login' run function login
+if (isset($_POST['action']) && $_POST['action'] == 'login') {
+    login($username, $password);
 }
+
+// if post 'action' is set to 'register' run function register
+if (isset($_POST['action']) && $_POST['action'] == 'register') {
+    register($username, $password);
+}
+
+  // define login function
+  function login($username, $password) {
+    global $connection;
+
+    // get user from database
+    $sql = "SELECT * FROM users WHERE email = '$username'";
+
+    $result = $connection->query($sql);
+
+    // if user exists
+    if ($result->num_rows > 0) {
+      // get user data
+      $user = $result->fetch_assoc();
+
+      // check if password is correct
+      if (password_verify($password, $user['password'])) {
+        // create session
+        session_start();
+        $_SESSION['user'] = $user['id'];
+
+        // return response
+        $response = array(
+          'status' => 'success',
+          'message' => 'Erfolgreich eingeloggt',
+        );
+      } else {
+        // return response
+        $response = array(
+          'status' => 'error',
+          'message' => 'Passwort ist falsch',
+        );
+      }
+    } else {
+      // return response
+      $response = array(
+        'status' => 'error',
+        'message' => 'Nutzer existiert nicht',
+      );
+    }
+
+    // return response
+    echo json_encode($response);
+  }
+
+  // define register function
+  function register($username, $password) {
+    global $connection;
+
+    // check if user exists
+    $sql = "SELECT * FROM users WHERE email = '$username'";
+
+    $result = $connection->query($sql);
+
+    // if user exists
+    if ($result->num_rows > 0) {
+      // return response
+      $response = array(
+        'status' => 'error',
+        'message' => 'Nutzer existiert bereits',
+      );
+    } else {
+      // hash password
+      $password = password_hash($password, PASSWORD_DEFAULT);
+
+      // insert user into database
+      $sql = "INSERT INTO users (email, password) VALUES ('$username', '$password')";
+
+      if ($connection->query($sql) === TRUE) {
+        // return response
+        $response = array(
+          'status' => 'success',
+          'message' => 'Nutzer wurde erfolgreich erstellt',
+        );
+      } else {
+        // return response
+        $response = array(
+          'status' => 'error',
+          'message' => 'Fehler: ' . $sql . '<br>' . $connection->error
+        );
+      }
+    }
+
+    // return response
+    echo json_encode($response);
+  }
+
+  // close connection
+  $connection->close();
+
+?>
